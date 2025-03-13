7f7b97d3f59e4577f4e4c5612435b68790457692
Guilherme <guilhermehenrique12308@gmail.com>
2025-03-12 23:46:41 -0300

    refactor: remove unnecessary mocks

--- src/components/Pages/home/StatusButton/__tests__/StatusButton.spec.tsx
+++ src/components/Pages/home/StatusButton/__tests__/StatusButton.spec.tsx
@@ -2,20 +2,6 @@ import { render, screen } from '@testing-library/react-native'
 import { StatusButton } from '../StatusButton'
 import { NetworkStatus } from '@/constants/network-status'
 
-jest.mock('@/constants/network-status', () => ({
-  ...jest.requireActual('@/constants/network-status'),
-  STATUS_COLORS: {
-    'loading': 'gray',
-    'online': 'green',
-    'offline': 'red',
-  },
-  NetworkStatus: {
-    LOADING: 'loading',
-    ONLINE: 'online',
-    OFFLINE: 'offline',
-  },
-}))
-
 describe('StatusButton component', () => {
   it('should be able to render ActivityIndicator when status is LOADING', () => {
     render(<StatusButton connectionStatus={NetworkStatus.LOADING} />)
@@ -23,25 +9,25 @@ describe('StatusButton component', () => {
     expect(screen.getByTestId('activity-indicator')).toBeTruthy()
 
     const button = screen.getByTestId('status-button')
-    expect(button).toHaveStyle({ backgroundColor: 'gray' })
+    expect(button).toHaveStyle({ backgroundColor: '#FFA500' })
   })
 
   it('should be able to ONLINE status text when status is ONLINE', () => {
     render(<StatusButton connectionStatus={NetworkStatus.ONLINE} />)
 
-    expect(screen.getByText('online')).toBeTruthy()
+    expect(screen.getByText('Você está ONLINE')).toBeTruthy()
 
     const button = screen.getByTestId('status-button')
-    expect(button).toHaveStyle({ backgroundColor: 'green' })
+    expect(button).toHaveStyle({ backgroundColor: '#008000' })
   })
 
   it('should be able to OFFLINE status text when status is OFFLINE', () => {
     render(<StatusButton connectionStatus={NetworkStatus.OFFLINE} />)
 
-    expect(screen.getByText('offline')).toBeTruthy()
+    expect(screen.getByText('Você está OFFLINE')).toBeTruthy()
 
     const button = screen.getByTestId('status-button')
-    expect(button).toHaveStyle({ backgroundColor: 'red' })
+    expect(button).toHaveStyle({ backgroundColor: '#FF0000' })
   })
 })
 
